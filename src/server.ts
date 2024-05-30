cc290972c522da5ad0e3bed6a917d277a778fc77
MAMUN <mamun@MAMUNs-MacBook-Pro.local>
2024-05-31 02:43:03 +0600

    basic-project

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import config from "./app/config";
+import app from "./app";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+}
+main();
