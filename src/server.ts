9cecc1934a12050df5e5e4863fd55150998d3211
MAMUN <mamun@MAMUNs-MacBook-Pro.local>
2024-05-31 02:58:10 +0600

    basic-project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import config from "./app/config";
-import app from "./app";
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
 
 async function main() {
   try {
@@ -8,7 +8,7 @@ async function main() {
 
     app.listen(config.port, () => {
       // eslint-disable-next-line no-console
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Server is listening on port ${config.port}`);
     });
   } catch (err) {
     // eslint-disable-next-line no-console
